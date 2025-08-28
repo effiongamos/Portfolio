07e494872adf953ee0476f35943598f4d5e3b6f5
effiongamos <beffiong3@byupathway.edu>
2025-08-28 17:07:34 +0100

    header

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// hamburger menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.getElementById('nav-links');
+
+  menuToggle.addEventListener('click', () => {
+    const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+    menuToggle.setAttribute('aria-expanded', !expanded);
+    navLinks.classList.toggle('open');
+  });
